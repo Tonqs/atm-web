f5f5b10784405991757634865d3e5d775cdd203b
Sarunya Siripirom <sarunya.si@ku.th>
2020-10-03 22:31:31 +0700

    added login.html added hash password
    added header, main layout

--- src/main/java/th/ac/ku/ATM/controller/CustomerController.java
+++ src/main/java/th/ac/ku/ATM/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.ATM.model.Customer;
+import th.ac.ku.ATM.service.CustomerService;
 
 
 @Controller
@@ -21,14 +23,14 @@ public class CustomerController {
     @GetMapping
     public String getCustomerPage(Model model){
 
-        model.addAttribute("allCustomer", customerService);
+        model.addAttribute("allCustomers", customerService.getCustomer());
         return "customer"; // customer.html
     }
 
     @PostMapping
     public String registerCustomer(@ModelAttribute Customer customer, Model model){
         customerService.createCustomer(customer);
-        model.addAttribute("allCustomer", customerService);
+        model.addAttribute("allCustomers", customerService.getCustomer());
         return "redirect:customer";
     }
 
